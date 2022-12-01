fb542cf9bbeab4feee701c2a7107f35f3de14136
Florent BERGE <fberge@lafourchette.com>
2022-12-01 14:51:31 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(elves: List<Elf>): Int {
+        return elves.maxBy { it.calories }.calories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(elves: List<Elf>): Int {
+        return elves.sortedByDescending { it.calories }
+            .take(3)
+            .sumOf { it.calories }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    val elves: MutableList<Elf> = mutableListOf()
+
+    var currentElf = 0
+    input.forEach {
+        if (it.isBlank()) {
+            elves.add(Elf(currentElf))
+            currentElf = 0
+        } else {
+            currentElf += it.toInt()
+        }
+    }
+    elves.add(Elf(currentElf))
+
+    println(part1(elves))
+    println(part2(elves))
 }
+data class Elf(val calories: Int)
