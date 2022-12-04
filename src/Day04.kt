304877faa5cd181fc14917d6fd5d4345b4a7d200
Florent BERGE <fberge@lafourchette.com>
2022-12-04 21:51:21 +0100

    Day 4

---
+++ src/Day04.kt
@@ -0,0 +1,30 @@
+fun main() {
+    data class Chore(val firstStart: Int, val firstEnd: Int, val secondStart: Int, val secondEnd: Int) {
+        val firstRange = firstStart.toInt()..firstEnd.toInt()
+        val secondRange = secondStart.toInt()..secondEnd.toInt()
+    }
+
+    fun part1(input: List<Chore>): Int {
+        return input.count {
+            it.secondRange.contains(it.firstStart) && it.secondRange.contains(it.firstEnd)
+                    || it.firstRange.contains(it.secondStart) && it.firstRange.contains(it.secondEnd)
+        }
+    }
+
+    fun part2(input: List<Chore>): Int {
+        return input.count {
+            it.secondRange.contains(it.firstStart) || it.secondRange.contains(it.firstEnd)
+                    || it.firstRange.contains(it.secondStart) || it.firstRange.contains(it.secondEnd)
+        }
+    }
+
+    val input = readInput("Day04").map {
+        val match = Regex("(\\d+)-(\\d+),(\\d+)-(\\d+)").find(it)!!
+        val (firstStart, firstEnd, secondStart, secondEnd) = match.destructured
+        Chore(firstStart.toInt(), firstEnd.toInt(), secondStart.toInt(), secondEnd.toInt())
+    }
+
+
+    println(part1(input))
+    println(part2(input))
+}
